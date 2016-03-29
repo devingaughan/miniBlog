8ce66009d74893a8bfa75f473717089f0940ee0e
unknown <devin@zovkov.com>
2016-03-29 10:47:48 -0700

    finished video 19 rendored login template

---
+++ public_html/js/admin.js
@@ -0,0 +1,25 @@
+$(function () {
+    var APPLICATION_ID = "72AB025C-DA9E-C720-FF74-AF6DEA817100",
+        SECRET_KEY = "574B6FB6-E894-BB40-FF6A-653373646F00",
+        VERSION = "v1";
+        
+    Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
+     
+     
+     
+     var loginScript = $("#login-template").html();
+     var loginTemplate = Handlebars.compile(loginScript);
+     
+     $('.main-container').html(loginTemplate);
+});
+
+function Posts(args) {
+    args = args || {};
+    this.title = args.title || "";
+    this.content = args.content || "";
+    this.authorEmail = args.authorEmail || "";
+}
+
+
+
+
